9f41a2c7cff714526bbfb8850959c8256755ff06
yinbp <yin-bp@163.com>
2019-01-18 23:49:09 +0800

    1.调整bboss版本号为5.2.0，bboss es版本号为5.3.5 2.改进数据同步工具：通过ImportBuilder和ESJDBC的addFieldValue方法添加的字段覆盖表中的字段值，通过context对象的addFieldValue方法添加的字段数据优先级最高

--- src/main/java/org/frameworkset/elasticsearch/imp/Dbdemo.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/Dbdemo.java
@@ -387,7 +387,7 @@ public class Dbdemo {
 		if(dropIndice) {
 			try {
 				//清除测试表,导入的时候回重建表，测试的时候加上为了看测试效果，实际线上环境不要删表
-				String repsonse = ElasticSearchHelper.getRestClientUtil().dropIndice("dbdemo");
+				String repsonse = ElasticSearchHelper.getRestClientUtil().dropIndice("cms_document");
 				System.out.println(repsonse);
 			} catch (Exception e) {
 			}
@@ -400,14 +400,14 @@ public class Dbdemo {
 		// log_id和数据库对应的字段一致,就不需要设置setNumberLastValueColumn和setNumberLastValueColumn信息，
 		// 但是需要设置setLastValueType告诉工具增量字段的类型
 
-		importBuilder.setSql("select * from td_sm_log ");
+		importBuilder.setSql("select * from td_cms_document ");
 //		importBuilder.setSql("select * from td_sm_log ");
 		/**
 		 * es相关配置
 		 */
 		importBuilder
-				.setIndex("dbdemo") //必填项
-				.setIndexType("dbdemo") //必填项
+				.setIndex("cms_document") //必填项
+				.setIndexType("cms_document") //必填项
 //				.setRefreshOption("refresh")//可选项，null表示不实时刷新，importBuilder.setRefreshOption("refresh");表示实时刷新
 				.setUseJavaName(true) //可选项,将数据库字段名称转换为java驼峰规范的名称，true转换，false不转换，默认false，例如:doc_id -> docId
 				.setUseLowcase(false)  //可选项，true 列名称转小写，false列名称不转换小写，默认false，只要在UseJavaName为false的情况下，配置才起作用
@@ -458,7 +458,7 @@ public class Dbdemo {
 //		importBuilder.setNumberLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 //		importBuilder.setNumberLastValueColumn("log_id");//手动指定日期增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 		importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
-		importBuilder.setLastValueStorePath("logtable_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
+		importBuilder.setLastValueStorePath("cmstable_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
 //		importBuilder.setLastValueStoreTableName("logs");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
 		importBuilder.setLastValueType(ImportIncreamentConfig.NUMBER_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
 		// 或者ImportIncreamentConfig.TIMESTAMP_TYPE 日期类型
@@ -486,23 +486,23 @@ public class Dbdemo {
 //		testObject.setId("testid");
 //		testObject.setName("jackson");
 //		importBuilder.addFieldValue("testObject",testObject);
-
+		importBuilder.addFieldValue("author","作者");
 		/**
 		 * 重新设置es数据结构
 		 */
 		importBuilder.setDataRefactor(new DataRefactor() {
 			public void refactor(Context context) throws Exception  {
-//				CustomObject customObject = new CustomObject();
-//				customObject.setAuthor((String)context.getValue("author"));
-//				customObject.setTitle((String)context.getValue("title"));
-//				customObject.setSubtitle((String)context.getValue("subtitle"));
-//				customObject.setIds(new int[]{1,2,3});
-//				context.addFieldValue("docInfo",customObject);//如果还需要构建更多的内部对象，可以继续构建
-				context.addIgnoreFieldMapping("remark1");
+				CustomObject customObject = new CustomObject();
+				customObject.setAuthor((String)context.getValue("author"));
+				customObject.setTitle((String)context.getValue("title"));
+				customObject.setSubtitle((String)context.getValue("subtitle"));
+				customObject.setIds(new int[]{1,2,3});
+				context.addFieldValue("author",customObject);//如果还需要构建更多的内部对象，可以继续构建
+//				context.addIgnoreFieldMapping("title");
 				//上述三个属性已经放置到docInfo中，如果无需再放置到索引文档中，可以忽略掉这些属性
 //				context.addIgnoreFieldMapping("author");
-//				context.addIgnoreFieldMapping("title");
-//				context.addIgnoreFieldMapping("subtitle");
+				context.addIgnoreFieldMapping("title");
+				context.addIgnoreFieldMapping("subtitle");
 			}
 		});
 		//映射和转换配置结束
@@ -515,7 +515,7 @@ public class Dbdemo {
 		importBuilder.setThreadCount(50);//设置批量导入线程池工作线程数量
 		importBuilder.setContinueOnError(true);//任务出现异常，是否继续执行作业：true（默认值）继续执行 false 中断作业执行
 		importBuilder.setAsyn(false);//true 异步方式执行，不等待所有导入作业任务结束，方法快速返回；false（默认值） 同步方式执行，等待所有导入作业任务结束，所有作业结束后方法才返回
-		importBuilder.setEsIdField("log_id");//设置文档主键，不设置，则自动产生文档id
+		importBuilder.setEsIdField("document_id");//设置文档主键，不设置，则自动产生文档id
 //		importBuilder.setDebugResponse(false);//设置是否将每次处理的reponse打印到日志文件中，默认false，不打印响应报文将大大提升性能，只有在调试需要的时候才打开，log日志级别同时要设置为INFO
 //		importBuilder.setDiscardBulkResponse(true);//设置是否需要批量处理的响应报文，不需要设置为false，true为需要，默认true，如果不需要响应报文将大大提升处理速度
 
@@ -566,6 +566,7 @@ public class Dbdemo {
 		// 但是需要设置setLastValueType告诉工具增量字段的类型
 
 		importBuilder.setSql("select * from td_sm_log ");
+
 //		importBuilder.setSql("select * from td_sm_log ");
 		/**
 		 * es相关配置
