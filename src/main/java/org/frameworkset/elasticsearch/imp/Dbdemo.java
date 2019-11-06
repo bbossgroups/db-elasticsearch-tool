901ca8957741d4eb3ebbfd72bfc7c9ed5df935aa
yinbp <yin-bp@163.com>
2019-11-06 19:17:14 +0800

    版本号调整为5.9.1

--- src/main/java/org/frameworkset/elasticsearch/imp/Dbdemo.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/Dbdemo.java
@@ -53,8 +53,8 @@ public class Dbdemo {
 		boolean dropIndice = true;//CommonLauncher.getBooleanAttribute("dropIndice",false);//同时指定了默认值
 //		dbdemo.fullImportData(  dropIndice);
 //		dbdemo.scheduleImportData(dropIndice);
-//		dbdemo.scheduleTimestampImportData(dropIndice);
-		dbdemo.scheduleImportData(dropIndice);
+		dbdemo.scheduleTimestampImportData(dropIndice);
+//		dbdemo.scheduleImportData(dropIndice);
 	}
 
 	/**
@@ -322,7 +322,7 @@ public class Dbdemo {
 		//增量配置开始
 //		importBuilder.setNumberLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 //		importBuilder.setDateLastValueColumn("log_id");//手动指定日期增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
-		importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
+		importBuilder.setFromFirst(false);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
 		importBuilder.setLastValueStorePath("logtable_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
 //		importBuilder.setLastValueStoreTableName("logs");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
 		importBuilder.setLastValueType(ImportIncreamentConfig.TIMESTAMP_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
