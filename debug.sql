2caf454f6b988dda39307d823a6f3a879305a25b
yinbp <yin-bp@163.com>
2020-03-26 21:27:29 +0800

    持久层改进： 1.改进dsl配置解析语法树缓存机制告警机制，缓存数量超过最大允许缓存size时，每missing 500次打印一次告警日志 2.sql配置文件property元素增加cacheSql控制参数，控制对应的语句是否需要缓存sql语句语法树 3.dsl配置property元素增加cacheDsl boolean控制参数，控制对应的语句是否需要缓存dsl语句语法树

--- debug.sql
+++ debug.sql
@@ -1,10 +1,10 @@
-INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2176, '|admin', '', '认证-管理', '113.12.192.230', '2019-10-31 23:48:55', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
-INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2177, 'admin', '', '认证管理', '125.71.166.254', '2019-10-31 23:48:56', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
-INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2178, 'admin', '', '认证管理', '125.71.166.254', '2019-10-31 23:48:56', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
-
+INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2200, '|admin', '', '认证-管理', '113.12.192.230', '2020-01-31 23:49:55', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
+INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2201, 'admin', '', '认证管理', '125.71.166.254', '2020-01-31 23:49:56', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
+INSERT INTO td_sm_log (LOG_ID, LOG_OPERUSER, OP_ORGID, OPER_MODULE, LOG_VISITORIAL, LOG_OPERTIME, LOG_CONTENT, REMARK1, OPER_TYPE) VALUES (2202, 'admin', '', '认证管理', '125.71.166.254', '2020-01-31 23:49:56', 'admin(系统管理员) 登陆[公共开发平台]', '', 1);
+update td_sm_log set testint=5
 select count(1) from batchtest
-
-
+select * from td_sm_log
+select max(log_id)+1 from td_sm_log
 select max(log_id) from td_sm_log
 select count(log_id) from td_sm_log
 select count(col1) from bigtable
