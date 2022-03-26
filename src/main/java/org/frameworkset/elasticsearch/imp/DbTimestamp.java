8fd1b37f02afbd6e3153c09d7cc494fa5946da61
yinbp <yin-bp@163.com>
2022-03-26 23:54:19 +0800

    完善db-es同步案例，增加时间戳增量同步案例

--- src/main/java/org/frameworkset/elasticsearch/imp/DbTimestamp.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/DbTimestamp.java
@@ -32,17 +32,17 @@ import org.slf4j.LoggerFactory;
 import java.util.Date;
 
 /**
- * <p>Description: 基于数字类型db-es增量同步案例，同步处理程序，如需调试同步功能，直接运行main方法即可
+ * <p>Description: 基于时间戳db-es增量同步案例，如需调试同步功能，直接运行main方法debug即可
  * <p></p>
  * <p>Copyright (c) 2018</p>
  * @Date 2018/9/27 20:38
  * @author biaoping.yin
  * @version 1.0
  */
-public class Dbdemo {
-	private static Logger logger = LoggerFactory.getLogger(Dbdemo.class);
+public class DbTimestamp {
+	private static Logger logger = LoggerFactory.getLogger(DbTimestamp.class);
 	public static void main(String args[]){
-		Dbdemo dbdemo = new Dbdemo();
+		DbTimestamp dbdemo = new DbTimestamp();
 		boolean dropIndice = true;//CommonLauncher.getBooleanAttribute("dropIndice",false);//同时指定了默认值
 //		dbdemo.fullImportData(  dropIndice);
 //		dbdemo.scheduleImportData(dropIndice);
@@ -60,7 +60,7 @@ public class Dbdemo {
 		if(dropIndice) {
 			try {
 				//清除测试表,导入的时候回重建表，测试的时候加上为了看测试效果，实际线上环境不要删表
-				String repsonse = ElasticSearchHelper.getRestClientUtil().dropIndice("dbdemo");
+				String repsonse = ElasticSearchHelper.getRestClientUtil().dropIndice("dbtimedemo");
 				System.out.println(repsonse);
 			} catch (Exception e) {
 			}
@@ -73,18 +73,15 @@ public class Dbdemo {
 		// log_id和数据库对应的字段一致,就不需要设置setLastValueColumn信息，
 		// 但是需要设置setLastValueType告诉工具增量字段的类型
 
-//		importBuilder.setSql("select * from td_sm_log where LOG_OPERTIME > #[LOG_OPERTIME]");
-		importBuilder.setSql("select * from td_sm_log where log_id > #[log_id]");
-//		importBuilder.addFieldMapping("LOG_CONTENT","message");
-//		importBuilder.addIgnoreFieldMapping("remark1");
-//		importBuilder.setSql("select * from td_sm_log ");
+		importBuilder.setSql("select * from td_sm_log where LOG_OPERTIME > #[LOG_OPERTIME]");
+
 		/**
 		 * es相关配置
 		 */
 //		importBuilder.setTargetElasticsearch("default,test");//同步数据到两个es集群
 		importBuilder.setTargetElasticsearch("test");
 		importBuilder
-				.setIndex("newdbdemo") //必填项
+				.setIndex("dbtimedemo") //必填项
 //				.setIndexType("dbdemo") //es 7以后的版本不需要设置indexType，es7以前的版本必需设置indexType
 //				.setRefreshOption("refresh")//可选项，null表示不实时刷新，importBuilder.setRefreshOption("refresh");表示实时刷新
 				.setUseJavaName(true) //可选项,将数据库字段名称转换为java驼峰规范的名称，true转换，false不转换，默认false，例如:doc_id -> docId
@@ -103,43 +100,27 @@ public class Dbdemo {
 		importBuilder.addCallInterceptor(new CallInterceptor() {
 			@Override
 			public void preCall(TaskContext taskContext) {
-				System.out.println("preCall");
+
 			}
 
 			@Override
 			public void afterCall(TaskContext taskContext) {
-				System.out.println("afterCall");
+				logger.info(taskContext.getJobTaskMetrics().toString());
 			}
 
 			@Override
 			public void throwException(TaskContext taskContext, Exception e) {
 				System.out.println("throwException");
 			}
-		}).addCallInterceptor(new CallInterceptor() {
-			@Override
-			public void preCall(TaskContext taskContext) {
-				System.out.println("preCall 1");
-			}
-
-			@Override
-			public void afterCall(TaskContext taskContext) {
-				System.out.println("afterCall 1");
-			}
-
-			@Override
-			public void throwException(TaskContext taskContext, Exception e) {
-				System.out.println("throwException 1");
-			}
 		});
 //		//设置任务执行拦截器结束，可以添加多个
 		//增量配置开始
 //		importBuilder.setStatusDbname("test");//设置增量状态数据源名称
-		importBuilder.setLastValueColumn("log_id");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
+		importBuilder.setLastValueColumn("LOG_OPERTIME");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
 		importBuilder.setFromFirst(false);//setFromfirst(false)，如果作业停了，作业重启后从上次截止位置开始采集数据，
 //		setFromfirst(true) 如果作业停了，作业重启后，重新开始采集数据
-		importBuilder.setLastValueStorePath("logtable_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
-		importBuilder.setLastValueStoreTableName("logs");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
-		importBuilder.setLastValueType(ImportIncreamentConfig.NUMBER_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
+		importBuilder.setLastValueStorePath("dbtimees_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
+		importBuilder.setLastValueType(ImportIncreamentConfig.TIMESTAMP_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
 //		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 //		try {
 //			Date date = format.parse("2000-01-01");
@@ -151,28 +132,6 @@ public class Dbdemo {
 		// 或者ImportIncreamentConfig.TIMESTAMP_TYPE 日期类型
 		//增量配置结束
 
-		//映射和转换配置开始
-//		/**
-//		 * db-es mapping 表字段名称到es 文档字段的映射：比如document_id -> docId
-//		 * 可以配置mapping，也可以不配置，默认基于java 驼峰规则进行db field-es field的映射和转换
-//		 */
-//		importBuilder.addFieldMapping("document_id","docId")
-//				.addFieldMapping("docwtime","docwTime")
-//				.addIgnoreFieldMapping("channel_id");//添加忽略字段
-//
-//
-//		/**
-//		 * 为每条记录添加额外的字段和值
-//		 * 可以为基本数据类型，也可以是复杂的对象
-//		 */
-//		importBuilder.addFieldValue("testF1","f1value");
-//		importBuilder.addFieldValue("testInt",0);
-//		importBuilder.addFieldValue("testDate",new Date());
-//		importBuilder.addFieldValue("testFormateDate","yyyy-MM-dd HH",new Date());
-//		TestObject testObject = new TestObject();
-//		testObject.setId("testid");
-//		testObject.setName("jackson");
-//		importBuilder.addFieldValue("testObject",testObject);
 //
 		/**
 		 * 重新设置es数据结构
@@ -215,7 +174,7 @@ public class Dbdemo {
 			@Override
 			public void success(TaskCommand<String,String> taskCommand, String result) {
 				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
-				logger.info(taskMetrics.toString());
+				logger.debug(taskMetrics.toString());
 				logger.debug(result);
 			}
 
@@ -223,13 +182,13 @@ public class Dbdemo {
 			public void error(TaskCommand<String,String> taskCommand, String result) {
 				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
 				logger.info(taskMetrics.toString());
-				logger.debug(result);
+				logger.warn(result);
 			}
 
 			@Override
 			public void exception(TaskCommand<String,String> taskCommand, Exception exception) {
 				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
-				logger.debug(taskMetrics.toString());
+				logger.warn(taskMetrics.toString(),exception);
 			}
 
 			@Override
