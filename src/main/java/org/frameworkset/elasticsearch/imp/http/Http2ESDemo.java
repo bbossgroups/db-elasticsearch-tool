c0007192ec36b6f9c4e3fbb6d66960454aa1dbbd
yinbp <yinbp@asiainfo.com>
2022-07-02 13:01:39 +0800

    数据同步改进：http input插件支持post和put两种模式的httpmethod 增加http input到elasticsearch的同步案例：分页和非分页案例
    mvc框架改进：增加对控制器异常输出到日志文件中

---
+++ src/main/java/org/frameworkset/elasticsearch/imp/http/Http2ESDemo.java
@@ -0,0 +1,256 @@
+package org.frameworkset.elasticsearch.imp.http;
+/**
+ * Copyright 2022 bboss
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.http.HttpResponse;
+import org.frameworkset.tran.DataRefactor;
+import org.frameworkset.tran.DataStream;
+import org.frameworkset.tran.ExportResultHandler;
+import org.frameworkset.tran.config.ImportBuilder;
+import org.frameworkset.tran.context.Context;
+import org.frameworkset.tran.metrics.TaskMetrics;
+import org.frameworkset.tran.plugin.es.output.ElasticsearchOutputConfig;
+import org.frameworkset.tran.plugin.http.input.HttpInputConfig;
+import org.frameworkset.tran.plugin.http.input.HttpRecord;
+import org.frameworkset.tran.schedule.CallInterceptor;
+import org.frameworkset.tran.schedule.ImportIncreamentConfig;
+import org.frameworkset.tran.schedule.TaskContext;
+import org.frameworkset.tran.task.TaskCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * <p>Description: </p>
+ * <p></p>
+ * <p>Copyright (c) 2020</p>
+ * @Date 2022/7/1
+ * @author biaoping.yin
+ * @version 1.0
+ */
+public class Http2ESDemo {
+	private static Logger logger = LoggerFactory.getLogger(Http2ESDemo.class);
+	public static void main(String[] args){
+
+
+		ImportBuilder importBuilder = new ImportBuilder() ;
+		importBuilder.setFetchSize(50).setBatchSize(10);
+		HttpInputConfig httpInputConfig = new HttpInputConfig();
+		//指定导入数据的dsl语句，必填项，可以设置自己的提取逻辑，
+		// 设置增量变量log_id，增量变量名称#[log_id]可以多次出现在sql语句的不同位置中，例如：
+
+
+		httpInputConfig.setDslFile("httpdsl.xml")
+				.setQueryDslName("queryDsl")
+				.setQueryUrl("/httpservice/getData.api")
+				.addSourceHttpPoolName("http.poolNames","datatran")
+				.addHttpInputConfig("datatran.http.health","/health")
+				.addHttpInputConfig("datatran.http.hosts","192.168.137.1:808")
+				.addHttpInputConfig("datatran.http.timeoutConnection","5000")
+				.addHttpInputConfig("datatran.http.timeoutSocket","50000")
+				.addHttpInputConfig("datatran.http.connectionRequestTimeout","50000")
+				.addHttpInputConfig("datatran.http.maxTotal","200")
+				.addHttpInputConfig("datatran.http.defaultMaxPerRoute","100")
+				.addHttpInputConfig("datatran.http.failAllContinue","true");
+
+		importBuilder.setInputConfig(httpInputConfig);
+		importBuilder.addParam("otherParam","陈雨菲2:0战胜戴资颖");
+
+
+//		importBuilder.addFieldMapping("LOG_CONTENT","message");
+//		importBuilder.addIgnoreFieldMapping("remark1");
+//		importBuilder.setSql("select * from td_sm_log ");
+		ElasticsearchOutputConfig elasticsearchOutputConfig = new ElasticsearchOutputConfig();
+		elasticsearchOutputConfig.setTargetElasticsearch("default")
+				.setIndex("https2es")
+				.setEsIdField("log_id")//设置文档主键，不设置，则自动产生文档id
+				.setDebugResponse(false)//设置是否将每次处理的reponse打印到日志文件中，默认false
+				.setDiscardBulkResponse(false);//设置是否需要批量处理的响应报文，不需要设置为false，true为需要，默认false
+		/**
+		 elasticsearchOutputConfig.setEsIdGenerator(new EsIdGenerator() {
+		 //如果指定EsIdGenerator，则根据下面的方法生成文档id，
+		 // 否则根据setEsIdField方法设置的字段值作为文档id，
+		 // 如果默认没有配置EsIdField和如果指定EsIdGenerator，则由es自动生成文档id
+
+		 @Override
+		 public Object genId(Context context) throws Exception {
+		 return SimpleStringUtil.getUUID();//返回null，则由es自动生成文档id
+		 }
+		 });
+		 */
+//				.setIndexType("dbdemo") ;//es 7以后的版本不需要设置indexType，es7以前的版本必需设置indexType;
+//				.setRefreshOption("refresh")//可选项，null表示不实时刷新，importBuilder.setRefreshOption("refresh");表示实时刷新
+		/**
+		 * es相关配置
+		 */
+//		elasticsearchOutputConfig.setTargetElasticsearch("default,test");//同步数据到两个es集群
+
+		importBuilder.setOutputConfig(elasticsearchOutputConfig);
+		importBuilder
+//
+				.setUseJavaName(true) //可选项,将数据库字段名称转换为java驼峰规范的名称，true转换，false不转换，默认false，例如:doc_id -> docId
+				.setUseLowcase(true)  //可选项，true 列名称转小写，false列名称不转换小写，默认false，只要在UseJavaName为false的情况下，配置才起作用
+				.setPrintTaskLog(true) //可选项，true 打印任务执行日志（耗时，处理记录数） false 不打印，默认值false
+				;  //可选项,批量导入es的记录数，默认为-1，逐条处理，> 0时批量处理
+
+		//定时任务配置，
+		importBuilder.setFixedRate(false)//参考jdk timer task文档对fixedRate的说明
+//					 .setScheduleDate(date) //指定任务开始执行时间：日期
+				.setDeyLay(1000L) // 任务延迟执行deylay毫秒后执行
+				.setPeriod(1000L * 60); //每隔period毫秒执行，如果不设置，只执行一次
+		//定时任务配置结束
+//
+//		//设置任务执行拦截器，可以添加多个，定时任务每次执行的拦截器
+		importBuilder.addCallInterceptor(new CallInterceptor() {
+			@Override
+			public void preCall(TaskContext taskContext) {
+				System.out.println("preCall");
+			}
+
+			@Override
+			public void afterCall(TaskContext taskContext) {
+				System.out.println("afterCall");
+			}
+
+			@Override
+			public void throwException(TaskContext taskContext, Exception e) {
+				System.out.println("throwException");
+			}
+		}).addCallInterceptor(new CallInterceptor() {
+			@Override
+			public void preCall(TaskContext taskContext) {
+				System.out.println("preCall 1");
+			}
+
+			@Override
+			public void afterCall(TaskContext taskContext) {
+				System.out.println("afterCall 1");
+			}
+
+			@Override
+			public void throwException(TaskContext taskContext, Exception e) {
+				System.out.println("throwException 1");
+			}
+		});
+//		//设置任务执行拦截器结束，可以添加多个
+		//增量配置开始
+//		importBuilder.setStatusDbname("test");//设置增量状态数据源名称
+		importBuilder.setLastValueColumn("logTime");//手动指定数字增量查询字段，默认采用上面设置的sql语句中的增量变量名称作为增量查询字段的名称，指定以后就用指定的字段
+		importBuilder.setFromFirst(false);//setFromfirst(false)，如果作业停了，作业重启后从上次截止位置开始采集数据，
+//		setFromfirst(true) 如果作业停了，作业重启后，重新开始采集数据
+		importBuilder.setStatusDbname("http2es");
+		importBuilder.setLastValueStorePath("http2es_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
+		importBuilder.setLastValueStoreTableName("http2es");//记录上次采集的增量字段值的表，可以不指定，采用默认表名increament_tab
+		importBuilder.setLastValueType(ImportIncreamentConfig.TIMESTAMP_TYPE);//如果没有指定增量查询字段名称，则需要指定字段类型：ImportIncreamentConfig.NUMBER_TYPE 数字类型
+		importBuilder.setIncreamentEndOffset(60*1);//单位：秒
+//		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//		try {
+//			Date date = format.parse("2000-01-01");
+//			importBuilder.setLastValue(date);//增量起始值配置
+//		}
+//		catch (Exception e){
+//			e.printStackTrace();
+//		}
+		// 或者ImportIncreamentConfig.TIMESTAMP_TYPE 日期类型
+		//增量配置结束
+
+		//映射和转换配置开始
+//		/**
+//		 * db-es mapping 表字段名称到es 文档字段的映射：比如document_id -> docId
+//		 * 可以配置mapping，也可以不配置，默认基于java 驼峰规则进行db field-es field的映射和转换
+//		 */
+//		importBuilder.addFieldMapping("document_id","docId")
+//				.addFieldMapping("docwtime","docwTime")
+//				.addIgnoreFieldMapping("channel_id");//添加忽略字段
+//
+//
+//		/**
+//		 * 为每条记录添加额外的字段和值
+//		 * 可以为基本数据类型，也可以是复杂的对象
+//		 */
+//		importBuilder.addFieldValue("testF1","f1value");
+//		importBuilder.addFieldValue("testInt",0);
+//		importBuilder.addFieldValue("testDate",new Date());
+//		importBuilder.addFieldValue("testFormateDate","yyyy-MM-dd HH",new Date());
+//		TestObject testObject = new TestObject();
+//		testObject.setId("testid");
+//		testObject.setName("jackson");
+//		importBuilder.addFieldValue("testObject",testObject);
+//
+		/**
+		 * 重新设置es数据结构
+		 */
+		importBuilder.setDataRefactor(new DataRefactor() {
+			public void refactor(Context context) throws Exception  {
+				long logTime = context.getLongValue("logTime");
+				context.addFieldValue("logTime",new Date(logTime));
+				long oldLogTime = context.getLongValue("oldLogTime");
+				context.addFieldValue("oldLogTime",new Date(oldLogTime));
+				long oldLogTimeEndTime = context.getLongValue("oldLogTimeEndTime");
+				context.addFieldValue("oldLogTimeEndTime",new Date(oldLogTimeEndTime));
+//				Date date = context.getDateValue("LOG_OPERTIME");
+
+				HttpRecord record = (HttpRecord) context.getCurrentRecord();
+				HttpResponse response = record.getResponse();//可以从httpresponse中获取head之类的信息
+				context.addFieldValue("collecttime",new Date());//添加采集时间
+
+			}
+		});
+		//映射和转换配置结束
+
+		/**
+		 * 内置线程池配置，实现多线程并行数据导入功能，作业完成退出时自动关闭该线程池
+		 */
+		importBuilder.setParallel(true);//设置为多线程并行批量导入,false串行
+		importBuilder.setQueue(10);//设置批量导入线程池等待队列长度
+		importBuilder.setThreadCount(50);//设置批量导入线程池工作线程数量
+		importBuilder.setContinueOnError(true);//任务出现异常，是否继续执行作业：true（默认值）继续执行 false 中断作业执行
+		importBuilder.setAsyn(false);//true 异步方式执行，不等待所有导入作业任务结束，方法快速返回；false（默认值） 同步方式执行，等待所有导入作业任务结束，所有作业结束后方法才返回
+
+		importBuilder.setExportResultHandler(new ExportResultHandler<String,String>() {
+			@Override
+			public void success(TaskCommand<String,String> taskCommand, String result) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.info(taskMetrics.toString());
+				logger.debug(result);
+			}
+
+			@Override
+			public void error(TaskCommand<String,String> taskCommand, String result) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.info(taskMetrics.toString());
+				logger.debug(result);
+			}
+
+			@Override
+			public void exception(TaskCommand<String,String> taskCommand, Exception exception) {
+				TaskMetrics taskMetrics = taskCommand.getTaskMetrics();
+				logger.debug(taskMetrics.toString());
+			}
+
+			@Override
+			public int getMaxRetry() {
+				return 0;
+			}
+		});
+		/**
+		 * 执行数据库表数据导入es操作
+		 */
+		DataStream dataStream = importBuilder.builder();
+		dataStream.execute();//执行导入操作
+	}
+}
